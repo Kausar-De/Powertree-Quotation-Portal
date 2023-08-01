6bd50e027f0de2b2c1a13e8b82934ec7a987e1f1
Kausar De <kde48business@gmail.com>
2023-08-01 16:46:01 +0530

    Changed allowed hosts

--- powerquote/settings.py
+++ powerquote/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-bxw*alx)41p%b@6vp5-ml+cr&inrjjtqnlajq2_d2%p84v%jva
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'powertree.pythonanywhere.com']
 
 
 # Application definition
